7a74e3e128e1639279112cecf39e50776852bbc3
ArunkiranT <95434893+ArunkiranT@users.noreply.github.com>
2021-12-10 12:20:43 +0530

    Add files via upload

---
+++ Dicee/index.js
@@ -0,0 +1,32 @@
+/*
+function getrandam1(){
+  var randomNumber1 = Math.floor(Math.random() * 6)+1;
+  return randomNumber1;
+}
+console.log(getrandam1());
+*/
+
+var randomNumber1 = Math.floor(Math.random()*6)+1; //1 - 6
+var randomDiceImage = "dice" + randomNumber1 +".png"; //dice1.png - dice6.png
+var randomImgSource ="Images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImgSource);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 ="Images/" + randomDiceImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1 > randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if(randomNumber1<randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "Player 2 Wins";  
+}
+else
+{
+  document.querySelector("h1").innerHTML = "Draw";
+}
